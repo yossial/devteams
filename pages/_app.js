a9ee6ac6172b59e2966959d1877c61460568fa55
yossi a <yossi.alfia@gmail.com>
2020-09-17 23:19:56 +0300

    Changed IconsAbout component due to props problem (in Next.js)

--- pages/_app.js
+++ pages/_app.js
@@ -13,3 +13,4 @@ export default class MyApp extends App {
     )
   }
 }
+
--- pages/index/sections/IconsAbout.js
+++ pages/index/sections/IconsAbout.js
@@ -1,27 +1,37 @@
 import React from 'react'
-import IconCard from './IconCard'
 import { FaRegLightbulb, FaAppStore, FaHandsHelping } from 'react-icons/fa';
 
 export default function IconsAbout() {
-  // const svg1 = () => { return (<svg width="70" height="70" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M7 3h10a4 4 0 110 8H7a4 4 0 110-8zm0 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA"></path><path d="M7 13h10a4 4 0 110 8H7a4 4 0 110-8zm10 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA" opacity=".3"></path></g></svg>) }
-  // const svg2 = () => { return (<svg width="70" height="70" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M5.5 4h4A1.5 1.5 0 0111 5.5v1A1.5 1.5 0 019.5 8h-4A1.5 1.5 0 014 6.5v-1A1.5 1.5 0 015.5 4zm9 12h4a1.5 1.5 0 011.5 1.5v1a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-1a1.5 1.5 0 011.5-1.5z" fill="#335EEA"></path><path d="M5.5 10h4a1.5 1.5 0 011.5 1.5v7A1.5 1.5 0 019.5 20h-4A1.5 1.5 0 014 18.5v-7A1.5 1.5 0 015.5 10zm9-6h4A1.5 1.5 0 0120 5.5v7a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-7A1.5 1.5 0 0114.5 4z" fill="#335EEA" opacity=".3"></path></g></svg>) }
-  // const svg3 = () => { return (<svg width="70" height="70" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M17.272 8.685a1 1 0 111.456-1.37l4 4.25a1 1 0 010 1.37l-4 4.25a1 1 0 01-1.456-1.37l3.355-3.565-3.355-3.565zm-10.544 0L3.373 12.25l3.355 3.565a1 1 0 01-1.456 1.37l-4-4.25a1 1 0 010-1.37l4-4.25a1 1 0 011.456 1.37z" fill="#335EEA"></path><rect fill="#335EEA" opacity=".3" transform="rotate(15 12 12)" x="11" y="4" width="2" height="16" rx="1"></rect></g></svg>) }
-
+  const iconCards = [
+    {
+      Icon: FaRegLightbulb,
+      title: "Make valueable ideas come to life",
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+      Icon: FaAppStore,
+      title: "Gain app development experience",
+      content: "Sequi non quia ab, ipsam consequuntur nesciunt"
+    },
+    {
+      Icon: FaHandsHelping,
+      title: "Contribute to the community",
+      content: "Natus sint dicta impedit doloremque tempore. Nemo!"
+    }
+  ]
   return (
     <section className="flex justify-evenly items-center md:justify-center flex-col 
     sm:flex-row" id="about">
-      <IconCard
-        Icon={FaRegLightbulb}
-        title="Take a part in great ideas"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit." />
-      <IconCard
-        Icon={FaAppStore}
-        title="Gain experience deceloping apps"
-        content="Sequi non quia ab, ipsam consequuntur nesciunt" />
-      <IconCard
-        Icon={FaHandsHelping}
-        title="Contribute to the community"
-        content="Natus sint dicta impedit doloremque tempore. Nemo!" />
-    </ section>
+      {iconCards.map(({ Icon, title, content }) =>
+        <div key={title} className="flex flex-col items-center max-w-sm overflow-hidden bg-gray-100 mx-2 py-4 md:my-0 text-center">
+          <Icon className="text-5xl text-center" />
+          <div className="px-6 pt-4">
+            <div className="font-bold text-xl mb-2">{title}</div>
+            <p className="text-gray-700 text-base">{content}</p>
+          </div>
+        </div>
+      )}
+
+    </section>
   )
 }
